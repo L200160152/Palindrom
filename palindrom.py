98aab506d03a5de25912104ccd3a5febb299e744
L200160152 <selviat33@gmail.com>
2019-05-03 22:05:25 +0700

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,8 +1,12 @@
 ##sevl02
 def Palindrom(string):
-    for i in range(1, len(string)):
+    a = 0
+    a = a+1
+    for i in range(a, len(string)):
         if string[i].lower() == string[-(i+1)].lower():
+            
             return True
+            
         else:
             return False
 
